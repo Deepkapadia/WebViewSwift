c409a9cc3f453e0f182bec4d4b21fee188662ec7
Deep_Kapadia <kapadia.deep12@gmail.com>
2019-01-17 17:07:16 +0530

    WebView_Swift
    Web View Basic Swift Code Sample

--- WebViewSwift/ViewController.swift
+++ WebViewSwift/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webv: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // for custom file display
+        var str=String();
+        str = Bundle.main.path(forResource: "ENERGYSTAR", ofType: "pdf")!;
+        let url = URL(string: str);
+        webv.loadRequest(URLRequest(url: url!));
+        
+        //for external web page
+        //let url2 = URL(string: "http://www.topsint.com/topserp/index.php?lg=1");
+        //webv.loadRequest(URLRequest(url: url2!));
     }
 
     override func didReceiveMemoryWarning() {
